d44a707fb36124630d53c77ffbeffe9c1306b3a7
DovguLucky <dovgluch98@gmail.com>
2023-02-10 12:11:59 +0200

    add:good job

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  let swich = 'on';
+  result = [];
+if(typeof matrix === typeof []) {
+  matrix.forEach(el => {
+    if (swich === 'on') {
+      el.forEach(e => {
+        result.push(e);
+        swich = 'rev';
+      })
+    } else {
+      el.reverse().forEach(e => {
+        result.push(e);
+        swich = 'on'
+      })
+    }
+  })
+}
+  return result;
 }
